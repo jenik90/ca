3341ad49e7e86e6a2897ce92fc716ea9320078d9
jeka90 <jenik90@tut.by>
2017-08-10 16:20:12 +0300

    hello

--- ConsoleApplication2/MyClass.cs
+++ ConsoleApplication2/MyClass.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2
 {
-    class Program
+    class MyClass
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello");
-        }
     }
 }
